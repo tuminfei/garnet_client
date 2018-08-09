8e8d30b8a8afb21d17ff4dfc8a940c24fa0818fa
tarzansos <tuminfei1981@gmail.com>
2018-08-09 16:28:16 +0800

    migrate

--- app/controllers/notify_controller.rb
+++ app/controllers/notify_controller.rb
@@ -11,70 +11,84 @@ module GarnetClient
 
     def deposits
       rs = request.body.read
-      msg = JSON.parse(rs)
+      code = '0'
+      begin
+        msg = JSON.parse(rs)
 
-      notice = GarnetClient::NoticeDeposit.new
-      notice.coin_type = msg['coin_type']
-      notice.contract_address = msg['contract_address']
-      notice.from = msg['from']
-      notice.tx_hash = msg['tx_hash']
-      notice.to = msg['to']
-      notice.value = msg['value']
-      notice.tx_timestamp = msg['timestamp']
-      notice.status = msg['status']
-      notice.notify_id = msg['notify_id']
-      notice.error_msg = msg['error_msg']
+        notice = GarnetClient::NoticeDeposit.new
+        notice.coin_type = msg['coin_type']
+        notice.contract_address = msg['contract_address']
+        notice.from = msg['from']
+        notice.tx_hash = msg['tx_hash']
+        notice.to = msg['to']
+        notice.value = msg['value']
+        notice.tx_timestamp = msg['timestamp']
+        notice.status = msg['status']
+        notice.notify_id = msg['notify_id']
+        notice.error_msg = msg['error_msg']
+        notice.save!
 
-      if notice.save
-        render text: {"status": {"code": 0}}
-      else
-        render text: {"status": {"code": 1}}
+      rescue JSON::ParserError => e
+        puts e.message
+        puts e.backtrace.inspect
+        code = '1'
       end
+
+      render text: '{"status": {"code": ' + code + '}}'
     end
 
     def withdraws
       rs = request.body.read
-      msg = JSON.parse(rs)
+      code = '0'
+      begin
+        msg = JSON.parse(rs)
 
-      notice = GarnetClient::NoticeWithdraw.new
-      notice.coin_type = msg['coin_type']
-      notice.contract_address = msg['contract_address']
-      notice.from = msg['from']
-      notice.tx_hash = msg['tx_hash']
-      notice.to = msg['to']
-      notice.value = msg['value']
-      notice.tx_timestamp = msg['timestamp']
-      notice.status = msg['status']
-      notice.notify_id = msg['notify_id']
-      notice.error_msg = msg['error_msg']
+        notice = GarnetClient::NoticeWithdraw.new
+        notice.coin_type = msg['coin_type']
+        notice.contract_address = msg['contract_address']
+        notice.from = msg['from']
+        notice.tx_hash = msg['tx_hash']
+        notice.to = msg['to']
+        notice.value = msg['value']
+        notice.tx_timestamp = msg['timestamp']
+        notice.status = msg['status']
+        notice.notify_id = msg['notify_id']
+        notice.error_msg = msg['error_msg']
+        notice.save!
 
-      if notice.save
-        render text: {"status": {"code": 0}}
-      else
-        render text: {"status": {"code": 1}}
+      rescue JSON::ParserError => e
+        puts e.message
+        puts e.backtrace.inspect
+        code = '1'
       end
+
+      render text: '{"status": {"code": ' + code + '}}'
     end
 
     def transactions
       rs = request.body.read
       msg = JSON.parse(rs)
+      code = '0'
 
-      notice = GarnetClient::NoticeTransaction.new
-      notice.coin_type = msg['coin_type']
-      notice.tx_id = msg['tx_id']
-      notice.tx_hash = msg['tx_hash']
-      notice.tx_timestamp = msg['timestamp']
-      notice.from = msg['from']
-      notice.to = msg['to']
-      notice.value = msg['value']
-      notice.contract_address = msg['contractAddress']
-      notice.status = msg['status']
-
-      if notice.save
-        render text: {"status": {"code": 0}}
-      else
-        render text: {"status": {"code": 1}}
+      begin
+        notice = GarnetClient::NoticeTransaction.new
+        notice.coin_type = msg['coin_type']
+        notice.tx_id = msg['tx_id']
+        notice.tx_hash = msg['tx_hash']
+        notice.tx_timestamp = msg['timestamp']
+        notice.from = msg['from']
+        notice.to = msg['to']
+        notice.value = msg['value']
+        notice.contract_address = msg['contractAddress']
+        notice.status = msg['status']
+        notice.save!
+      rescue JSON::ParserError => e
+        puts e.message
+        puts e.backtrace.inspect
+        code = '1'
       end
+
+      render text: '{"status": {"code": ' + code + '}}'
     end
   end
 end
